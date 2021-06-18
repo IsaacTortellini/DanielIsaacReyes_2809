c99c8c3ce6d45de0ccd707b2d0456f5ca611cf21
IsaacTortellini <daniel.isaac.2503@gmail.com>
2021-06-17 22:06:04 -0500

    Las capturas se encuentran en el archivo con extensión .docx

---
+++ Tarea6/hola.c
@@ -0,0 +1,12 @@
+/*Archivo de  ejemplo Tarea6*/
+
+#include <conio.h>
+#include <stdio.h>
+// cambio
+int main()
+{
+	printf("Hola mundo, este es un codigo sencillo utilizado en mi tarea6");
+
+	return 0;
+}
+
